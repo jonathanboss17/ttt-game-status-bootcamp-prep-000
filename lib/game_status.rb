b31366f3f644efe400ba02efd133e7f89cba320f
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-15 06:09:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,17 +28,13 @@ WIN_COMBINATIONS = [
     
     WIN_COMBINATIONS.each do |i|
       i.each do |j|
-        
         if(board[j] == "X")
           x_count+=1
         end
-        
         if(board[j] == "O")
           o_count+=1 
         end
-        
       end
-      
       if(x_count == 3 || o_count == 3)
         return i.to_ary
         break
@@ -46,15 +42,14 @@ WIN_COMBINATIONS = [
         x_count = 0 
         o_count = 0
       end
-      
     end
     
     if(x_count != 3 && o_count != 3)
       return false 
     end
     
-    board.each do |a|
-      if(a = " ")
+    board.each do |i|
+      if(i == " ")
         return false
       end
     end
