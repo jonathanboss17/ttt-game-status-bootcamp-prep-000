7a2f0486e9c3edb15324046b65dc96eda2d5aab1
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-15 05:06:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,33 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  #horizontal
+  [0, 1, 2], 
+  [3, 4, 5], 
+  [6, 7, 8],
+  
+  #vertical
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  
+  #diagonal
+  [0, 4, 8], 
+  [2, 4, 6],
+  ]
+  
+  def won?(board)
+    count = 0 
+    WIN_COMBINATIONS.each do |i| 
+      i.each do |j|
+        if(board[j] == "X")
+          count+=1 
+        end 
+        
+        if(count == 3)
+        
+      end
+    end
+  end
