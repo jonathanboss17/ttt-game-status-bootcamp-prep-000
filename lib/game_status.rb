94c43e5dd9ca1f55542a16839a93638dbe2a40ba
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-15 06:49:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -86,10 +86,10 @@ WIN_COMBINATIONS = [
   end
   
   def winner(board)
-    if(won?(board) == false)
-      return false 
-    else
+    if(won?(board) == true)
       return board[won?(board)[0]]
+    else
+      return false
     end
   end
   
